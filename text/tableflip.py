2e62131504bc8fc6c66daaa4c4f70d2c63f88d92
Guaiyu <38230797+Guaiyu11@users.noreply.github.com>
2026-04-13 16:28:23 +0200

    feat: reorganize tableflip.py into text/

---
+++ text/tableflip.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Tableflip - Generate text emoticons like (╯°□°）╯︵ ┻━┻
+Usage: python tableflip.py [text]
+"""
+
+import sys
+
+TABLEFLIPS = [
+    '(╯°□°）╯︵ ┻━┻',
+    '(ノಠ益ಠ)ノ彡┻━┻',
+    '┻━┻ ︵ヽ(`Д´)ノ︵ ┻━┻',
+    '(ノಠ益ಠ)ノ彡︵ table',
+    '┬─┬ノ( º _ ºノ) ︵ ┻━┻',
+    '(╯°□°)╯︵ ┻━━━━┻',
+    '(ノಠ益ಠ)ノ彡┻━━━┻',
+    '┻━┻︵└(՞▃՞└)',
+]
+
+def flip(text='┻━┻', emoticon='(╯°□°）╯'):
+    """Generate tableflip."""
+    return f"{emoticon︵ {text}"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(flip(sys.argv[1]))
+    else:
+        for tf in TABLEFLIPS:
+            print(tf)
